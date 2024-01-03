9ca1afe61c127b4eb117fa94ab1b0d23e331f92d
BoburBerdiev <sirofim012@gmail.com>
2024-01-03 16:31:27 +0500

    Video section

--- src/pages/li-mega.jsx
+++ src/pages/li-mega.jsx
@@ -1,4 +1,5 @@
-import {IndexBanner} from "@/components";
+import {IndexBanner, SectionTitle, Video} from "@/components";
+import {BsCaretRightFill} from "react-icons/bs";
 
 
 const liMega = ({ children }) => {
@@ -11,6 +12,28 @@ const liMega = ({ children }) => {
                 'w-full h-screen'
             }>
                 <IndexBanner carHeader={true} />
+            </section>
+            <section className={'relative section'}>
+                <div className={'container'}>
+                    <div className={'grid grid-cols-5 '}>
+                        <div className={'col-span-5 md:col-span-3'}>
+                            <SectionTitle styleBox={'text-start'} title={'Li MEGA, The greatest happiness!'} subSmallTitle={'Li MEGA launch will be officially released on March 1, 2024.'} />
+                            <button className={'flex gap-x-2 '}>
+
+                                <span>
+                                Watch Video
+                                </span>
+                                <span>
+                                    <BsCaretRightFill/>
+                                </span>
+                            </button>
+                        </div>
+                        <div className={'col-span-5 h-screen'}>
+                            <Video />
+                        </div>
+                    </div>
+
+                </div>
             </section>
         </div>
     );

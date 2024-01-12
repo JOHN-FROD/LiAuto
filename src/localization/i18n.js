d89e706aacc17c414abebe9813bd128578e619f3
BoburBerdiev <sirofim012@gmail.com>
2024-01-12 11:53:39 +0500

    i18 ulangan

---
+++ src/localization/i18n.js
@@ -0,0 +1,17 @@
+import {initReactI18next} from "react-i18next";
+import i18next from "i18next";
+import uz from './uz.json'
+import ru from './ru.json'
+
+
+i18next.use(initReactI18next).init({
+    fallbackLng: 'ru',
+    supportedLngs: ['ru', 'uz'],
+    interpolation: {
+        escapeValue: false
+    },
+    resources: {
+        ru,
+        uz
+    }
+})

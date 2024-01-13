2322004d73530ecafe6efe5cac95050d1ef61ec1
islom <iabdugofurov99@gmail.com>
2024-01-13 11:37:40 +0500

    pull uchun

--- src/components/swiper-section/swiper-section.jsx
+++ src/components/swiper-section/swiper-section.jsx
@@ -4,14 +4,12 @@ import {useEffect, useState} from "react";
 import Slide from "@/components/swiper-section/Slide";
 import Aos from 'aos'
 
-const SwiperSection = ({carousel, hoverChangeText,video,container}) => {
+const SwiperSection = ({carousel, hoverChangeText, video, container}) => {
     const [paginationGrid, setPaginationGrid] = useState(0)
     const [swiper, setSwiper] = useState(null);
     const [activeIndex, setActiveIndex] = useState(0)
     const [resizeWidth, setResizeWidth] = useState(typeof window !== 'undefined' && window.innerWidth <= 640)
     const [isRefresh, setIsRefresh] = useState(false)
-
-
 
 
     const handlePaginationClick = (index) => {
@@ -34,7 +32,11 @@ const SwiperSection = ({carousel, hoverChangeText,video,container}) => {
 
 
     useEffect(() => {
-        Aos.init({ duration: 500 });
+        Aos.init({
+            duration: 500,
+            // once: true
+        });
+
     }, []);
 
     useEffect(() => {
@@ -62,7 +64,8 @@ const SwiperSection = ({carousel, hoverChangeText,video,container}) => {
     }, [resizeWidth]);
 
     return (
-        <div className={`${container ? "container" : "max-sm:container"} max-sm:container h-auto ${video ? " " :"sm:h-[50vh] lg:h-screen"}  w-full relative`}>
+        <div
+            className={`${container ? "container" : "max-sm:container"} max-sm:container h-auto ${video ? " " : "sm:h-[50vh] lg:h-screen"}  w-full relative`}>
             <Swiper
                 onSwiper={(swiper) => setSwiper(swiper)}
                 onSnapIndexChange={(swiperCore) => handleSlideChange(swiperCore)}
@@ -105,7 +108,7 @@ const SwiperSection = ({carousel, hoverChangeText,video,container}) => {
             <div
                 className={`static sm:absolute left-0 bottom-10 md:bottom-20 z-10 w-full ${hoverChangeText ? "" : "sm:block hidden"}`}>
                 <div
-                    className={`${container ? "container sm:px-10":"container"} grid ${hoverChangeText ? "gap-2 sm:gap-0" : "gap-6 md:gap-10"}   ${paginationGrid === 2 ? "px-10 lg:px-20" : "px-0"}`}
+                    className={`${container ? "container sm:px-10" : "container"} grid ${hoverChangeText ? "gap-2 sm:gap-0" : "gap-6 md:gap-10"}   ${paginationGrid === 2 ? "px-10 lg:px-20" : "px-0"}`}
                     style={{gridTemplateColumns: `repeat(${paginationGrid},1fr)`}}
 
                 >
@@ -120,19 +123,30 @@ const SwiperSection = ({carousel, hoverChangeText,video,container}) => {
                             {
                                 hoverChangeText
                                     ?
-                                    <div className={'flex justify-items-start sm:justify-between  sm:flex-col flex-row gap-2 h-full '}>
-                                        <h6 className={`order-2 sm:order-1 text-sm md:text-lg lg:text-xl text-center duration-300 ${activeIndex === index ? 'text-black sm:text-white' : ' text-black/40 sm:text-white/40 '}`}>
+                                    <div
+                                        className={'flex justify-items-start sm:justify-between  sm:flex-col flex-row gap-2 h-full '}>
+                                        <div data-aos={'fade-up'}
+                                             data-aos-duration="300"
+                                        >
+
+                                        <h6  className={`order-2 sm:order-1 text-sm md:text-lg lg:text-xl text-center duration-300 ${activeIndex === index ? 'text-black sm:text-white' : ' text-black/40 sm:text-white/40 '}`}>
                                             {item.title}
                                         </h6>
-                                        <div
-                                            data-aos={'fade-right'}
-                                            className={`order-1 sm:order-2  w-[1px] h-full sm:w-full sm:h-0.5 duration-700  ${activeIndex === index ? 'bg-currentRed' : 'bg-white/40'}`}></div>
+                                        </div>
+                                        <div data-aos={'fade-right'}>
+                                            <div
+
+                                                className={`order-1 sm:order-2  w-[1px] h-full sm:w-full sm:h-0.5 duration-700  ${activeIndex === index ? 'bg-currentRed' : 'bg-white/40'}`}></div>
+                                        </div>
 
                                     </div>
                                     :
                                     <>
+                                        <div data-aos={'fade-right'}>
+
                                         <div
                                             className={`w-full h-0.5  ${activeIndex === index ? 'bg-currentGold' : 'bg-white/40'}`}></div>
+                                        </div>
                                         <h6 className={` text-xs xl:text-sm xxl:text-base  ${activeIndex === index ? 'text-white' : 'text-white/40 '}`}>
                                             {item.title}
                                         </h6>
@@ -145,7 +159,8 @@ const SwiperSection = ({carousel, hoverChangeText,video,container}) => {
                         </div>
 
                     ))}
-                    <div className={`${hoverChangeText ? '' :'hidden'} order-first sm:order-last col-span-full text-start sm:text-center mb-3 sm:mb-0 mt-0 sm:mt-3 text-xs xxl:text-sm text-black sm:text-white`}>
+                    <div
+                        className={`${hoverChangeText ? '' : 'hidden'} order-first sm:order-last col-span-full text-start sm:text-center mb-3 sm:mb-0 mt-0 sm:mt-3 text-xs xxl:text-sm text-black sm:text-white`}>
                         {carousel[activeIndex]?.text}
                     </div>
                 </div>

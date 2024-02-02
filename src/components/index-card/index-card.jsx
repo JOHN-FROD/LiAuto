6da7bd50f3a7549f73d926d053bff2ac5550ad2d
zokirjonovshohruh <shohruhfrontend@gmail.com>
2024-02-02 20:09:11 +0500

    about page add 50%

--- src/components/index-card/index-card.jsx
+++ src/components/index-card/index-card.jsx
@@ -1,35 +1,39 @@
 import {ImageUI} from "@/components";
 import {FaAngleRight} from "react-icons/fa";
-const IndexCard = ({contentPositionEnd , bottomText , about, title, subtitle, bg, bgRes, btnText}) => {
+const IndexCard = ({contentPositionEnd , bottomText , about, title, subtitle, bg, bgRes, btnText, isAboutGrid}) => {
     // contentPosition ---- start or end
 
 
     return (
-        <div className={`relative flex flex-col ${contentPositionEnd === 'justify-end' ? 'justify-end' : contentPositionEnd}  p-5 py-10 items-center h-full   `}>
+        <div className={`relative flex flex-col ${contentPositionEnd === 'justify-end' ? 'justify-end' : contentPositionEnd}  p-5 py-10 items-center h-full ${about && 'rounded overflow-hidden'}  `}>
             <ImageUI src={bg} alt={'mega'} imgStyle={'object-cover hidden  md:block object-center z-[1]'}/>
             <ImageUI src={bgRes} alt={'mega'} imgStyle={'object-cover block md:hidden object-center  z-[1]'}/>
             <div className={'relative   z-[5]'}>
-                <div className={'text-white flex flex-col justify-center items-center   gap-y-2 md:gap-y-4 text-center'}>
-                    <h3 className={'text-xl md:text-2xl lg:text-3xl font-bold'}>
+                <div className={`text-white flex flex-col justify-center items-center   ${isAboutGrid ? 'gap-y-2' : 'gap-y-2 md:gap-y-4'} text-center`}>
+                    <h3 className={` ${isAboutGrid ? ' lg:text-lg' : 'text-xl md:text-2xl lg:text-3xl '} ${about ? "font-medium" : "font-bold "} `}>
                         {title}
                     </h3>
-                    <p className={`text-sm md:text-base ${about ? 'text-darkText' : ''}  lg:text-xl `}>
+                    <p className={` ${about ? 'text-[#ffffffb3] text-sm' : 'text-sm md:text-base lg:text-xl'}   `}>
                         {subtitle}
                     </p>
+                    {
+                        btnText &&
+
                     <button
-                        className={'text-white  duration-200 flex items-center gap-x-1.5 text-sm md:text-base'}>
+                        className={`text-white  duration-200 flex items-center gap-x-1.5 ${about ? "text-xs " : "text-sm md:text-base"} `}>
                     <span>
                         {btnText}
                     </span>
                         <FaAngleRight/>
                     </button>
+                    }
                 </div>
 
             </div>
             {
                 bottomText &&
             <div className={'relative z-[5] text-center'}>
-                <p className={'text-base text-darkText'}>
+                <p className={`text-base ${about ? "text-[#ffffff80]" : " text-darkText"}`}>
                     {bottomText}
                 </p>
             </div>
--- src/components/list-car/list-car.jsx
+++ src/components/list-car/list-car.jsx
@@ -13,7 +13,7 @@ const ListCar = ({  title  , listCenter , subTitle , list , bgRes , bg ,about}) 
 
     }, []);
     return (
-        <div className={'relative flex items-start justify-center h-full pt-[20%]  md:pt-[15%] lg:pt-[10%] '}>
+        <div className={`relative flex items-start justify-center h-full pt-[20%]  ${about ? 'md:pt-[10%] lg:pt-[7%]' : " md:pt-[15%] lg:pt-[10%]"} `}>
             <ImageUI src={bg} alt={'mega'} imgStyle={'object-cover hidden md:block object-center z-[1]'}/>
             <ImageUI src={bgRes} alt={'mega'} imgStyle={'object-cover block md:hidden object-center z-[1]'}/>
             <div className={'relative container z-[5] flex flex-col justify-center items-center gap-4-6 text-white'}>
@@ -21,25 +21,25 @@ const ListCar = ({  title  , listCenter , subTitle , list , bgRes , bg ,about}) 
                 <SectionTitle darkMode={about ? false : true} black={true}
                               title={title}/>
 
-                <p className={`text-sm md:text-lg mt-3   text-black text-center`}>
+                <p className={`text-sm  mt-3 ${about ? 'md:text-sm lg:px-[18vw]' : 'md:text-lg'}   text-black text-center`}>
                     {subTitle}
                 </p>
 
 
                 <div className={'xl:w-2/3'} data-aos={'fade-up'} data-aos-duration="600">
                     <div
-                        className={`grid  ${about ? 'grid-cols-1 md:grid-cols-3 gap-10 text-black' : 'grid-cols-2 gap-6 md:grid-cols-4'}    md:grid-cols-4 md:divide-x  ${about ? 'divide-black': 'divide-white/30'}  my-4`}>
+                        className={`w-full  ${about ? 'flex flex-wrap gap-y-10 items-center justify-center text-black' : 'grid grid-cols-2 gap-6 md:grid-cols-4'}    md:grid-cols-4 md:divide-x  ${about ? 'divide-black': 'divide-white/30'}  my-4`}>
                     {
                             list?.map((item, ind) => (
                                 <div key={ind}
-                                     className={`flex justify-center items-center w-full h-full`}>
-                                    <div className={`px-1 flex flex-col justify-between  ${listCenter ? 'text-center' : 'text-left'}`}>
-                                        <p className={` md:text-xs lg:text-base ${about ? 'order-2' : 'order-1 text-base'}`}>
+                                     className={`flex justify-center items-center `}>
+                                    <div className={`px-1 flex flex-col justify-between ${about ? "px-[2vw]" : ""}   ${listCenter ? 'text-center' : 'text-left'}`}>
+                                        <p className={` md:text-xs lg:text-base ${about ? 'order-2 lg:text-sm ' : 'order-1 text-base'}`}>
                                             {
                                                 item.name
                                             }
                                         </p>
-                                        <p className={`  md:text-3xl lg:text-5xl font-medium md:font-normal ${about ? 'order-1 text-xl' : 'order-2 text-2xl'}`}>
+                                        <p className={`  md:text-3xl lg:text-5xl font-medium md:font-normal ${about ? 'order-1 md:text-lg lg:text-xl' : 'order-2 text-2xl'}`}>
                                             {
                                                 item.number
                                             }
